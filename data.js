58ad0f73c21a4e5e805bb5b20de0574ce8e2e9ca
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2025-11-30 21:29:46 +0530

    Update fee structure for multiple classes and years
    Revised the 'fees' and 'term' values for several classes across the years 2023, 2024, and 2025 in the manualFees object. This update reflects new fee amounts and corrects previous values, including a fix for the 'Pre KG' term fee in 2025.

--- data.js
+++ data.js
@@ -5,160 +5,160 @@ const manualFees = {
     {
       "age": 3,
       "class": "Pre KG",
-      "fees": 24000,
-      "term": 6000
+      "fees": 0,
+      "term": 0
     },
     {
       "age": 4,
       "class": "LKG",
-      "fees": 27000,
-      "term": 6750
+      "fees": 22000,
+      "term": 55500
     },
     {
       "age": 5,
       "class": "UKG",
-      "fees": 27000,
-      "term": 6750
+      "fees": 22000,
+      "term": 55000
     },
     {
       "age": 6,
       "class": "I",
-      "fees": 31500,
-      "term": 7750
+      "fees": 26000,
+      "term": 6500
     },
     {
       "age": 7,
       "class": "II",
-      "fees": 31500,
-      "term": 7750
+      "fees": 26000,
+      "term": 6500
     },
     {
       "age": 8,
       "class": "III",
-      "fees": 32700,
-      "term": 8050
+      "fees": 27000,
+      "term": 6750
     },
     {
       "age": 9,
       "class": "IV",
-      "fees": 32700,
-      "term": 8050
+      "fees": 27000,
+      "term": 6750
     },
     {
       "age": 10,
       "class": "V",
-      "fees": 33900,
-      "term": 8350
+      "fees": 28000,
+      "term": 7000
     },
     {
       "age": 11,
       "class": "VI",
-      "fees": 33900,
-      "term": 8350
+      "fees": 28000,
+      "term": 7000
     },
     {
       "age": 12,
       "class": "VII",
-      "fees": 35100,
-      "term": 8650
+      "fees": 29000,
+      "term": 7250
     },
     {
       "age": 13,
       "class": "VIII",
-      "fees": 43500,
-      "term": 10750
+      "fees": 36000,
+      "term": 9000
     },
     {
       "age": 14,
       "class": "IX",
-      "fees": 45900,
-      "term": 11350
+      "fees": 38000,
+      "term": 9500
     },
     {
       "age": 15,
       "class": "X",
-      "fees": 45400,
-      "term": 11350
+      "fees": 38000,
+      "term": 9500
     }
   ],
   "2024": [
     {
       "age": 3,
       "class": "Pre KG",
-      "fees": 24000,
-      "term": 6000
+      "fees": 10500,
+      "term": 5250
     },
     {
       "age": 4,
       "class": "LKG",
-      "fees": 27000,
-      "term": 6750
+      "fees": 24200,
+      "term": 6050
     },
     {
       "age": 5,
       "class": "UKG",
-      "fees": 27000,
-      "term": 6750
+      "fees": 24200,
+      "term": 6050
     },
     {
       "age": 6,
       "class": "I",
-      "fees": 31500,
-      "term": 7750
+      "fees": 29000,
+      "term": 7150
     },
     {
       "age": 7,
       "class": "II",
-      "fees": 31500,
-      "term": 7750
+      "fees": 29000,
+      "term": 7150
     },
     {
       "age": 8,
       "class": "III",
-      "fees": 32700,
-      "term": 8050
+      "fees": 30100,
+      "term": 7425
     },
     {
       "age": 9,
       "class": "IV",
-      "fees": 32700,
-      "term": 8050
+      "fees": 300100,
+      "term": 7425
     },
     {
       "age": 10,
       "class": "V",
-      "fees": 33900,
-      "term": 8350
+      "fees": 31200,
+      "term": 7700
     },
     {
       "age": 11,
       "class": "VI",
-      "fees": 33900,
-      "term": 8350
+      "fees": 31200,
+      "term": 7700
     },
     {
       "age": 12,
       "class": "VII",
-      "fees": 35100,
-      "term": 8650
+      "fees": 32300,
+      "term": 7975
     },
     {
       "age": 13,
       "class": "VIII",
-      "fees": 43500,
-      "term": 10750
+      "fees": 40000,
+      "term": 9900
     },
     {
       "age": 14,
       "class": "IX",
-      "fees": 45900,
-      "term": 11350
+      "fees": 41800,
+      "term": 10450
     },
     {
       "age": 15,
       "class": "X",
-      "fees": 45400,
-      "term": 11350
+      "fees": 41800,
+      "term": 10450
     }
   ],
   "2025": [
@@ -166,7 +166,7 @@ const manualFees = {
       "age": 3,
       "class": "Pre KG",
       "fees": 24000,
-      "term": 6750
+      "term": 6000
     },
     {
       "age": 4,
